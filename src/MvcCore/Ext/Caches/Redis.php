74e0183ebd188a4214c69059048b82e8213fd725
Tom Flídr <tomflidr@gmail.com>
2021-01-15 22:46:10 +0100

    Fixed copyright heading, fixed php docs comments with unnecessary interfaces, upgraded version constant to 5.0.0

--- src/MvcCore/Ext/Caches/Redis.php
+++ src/MvcCore/Ext/Caches/Redis.php
@@ -7,14 +7,21 @@
  * For the full copyright and license information, please view
  * the LICENSE.md file that are distributed with this source code.
  *
- * @copyright	Copyright (c) 2016 Tom Flídr (https://github.com/mvccore/mvccore)
+ * @copyright	Copyright (c) 2016 Tom Flidr (https://github.com/mvccore)
  * @license  https://mvccore.github.io/docs/mvccore/5.0.0/LICENCE.md
  */
 
 namespace MvcCore\Ext\Caches;
 
-class Redis implements \MvcCore\Ext\ICache
-{
+class Redis implements \MvcCore\Ext\ICache {
+	
+	/**
+	 * MvcCore Extension - Cache - Redis - version:
+	 * Comparison by PHP function version_compare();
+	 * @see http://php.net/manual/en/function.version-compare.php
+	 */
+	const VERSION = '5.0.0';
+
 	/** @var array */
 	protected static $instances = [];
 
